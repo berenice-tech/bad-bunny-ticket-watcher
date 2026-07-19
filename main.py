5acb43977b5b7fd00d1c4287a94ae93455e452d5
berenice-tech <berenice.magloire@gmail.com>
2026-07-19 15:43:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,21 @@ def lancer_surveillance():
 
     tickets = recuperer_vip()
 
-    for ticket in tickets:
+
+
+for ticket in tickets:
+
+    analyse = scorer_vip(
+        ticket["package"],
+        ticket["prix"]
+    )
+
+    ticket["score"] = analyse["score"]
+    ticket["niveau"] = analyse["niveau"]
+
+    if ticket["score"] > 0:
         resultat = traiter_ticket(ticket)
         print(resultat)
-
 
 if __name__ == "__main__":
     lancer_surveillance()
