6f1ff9c5c0daf1b187c56add64e1850cea633922
berenice-tech <berenice.magloire@gmail.com>
2026-07-19 20:21:28 +0200

    Update ticketmaster_vip.py

--- ticketmaster_vip.py
+++ ticketmaster_vip.py
@@ -1,6 +1,21 @@
 import requests
 
-VIP_URL = "https://www.ticketmaster.be/event/bad-bunny-debi-tirar-mas-fotos-world-tour-%7C-vip-packages-tickets/1339346645?language=fr-be"
+
+HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 "
+        "(KHTML, like Gecko) "
+        "Chrome/126.0 Safari/537.36"
+    )
+}
+
+
+VIP_URL = (
+    "https://www.ticketmaster.be/event/"
+    "bad-bunny-debi-tirar-mas-fotos-world-tour-%7C-vip-packages-tickets/1339346645"
+    "?language=fr-be"
+)
 
 
 PACKAGES_SURVEILLES = [
