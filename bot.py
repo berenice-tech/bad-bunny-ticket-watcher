1bf3b0f1091d0748ca922de880dbbb0cae2dd69a
berenice-tech <berenice.magloire@gmail.com>
2026-07-19 11:34:10 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,17 @@
-print("Bad Bunny Ticket Watcher démarré 🎟️")
+import os
+import requests
+
+TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
+
+def envoyer_message(message):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    data = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+    requests.post(url, data=data)
+
+if __name__ == "__main__":
+    envoyer_message("🎟️ Bad Bunny Ticket Watcher connecté !")
+    print("Message Telegram envoyé")
