bc7fdb14f6cd294613be752a167cc457869788aa
berenice-tech <berenice.magloire@gmail.com>
2026-07-19 15:20:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
-from bot import *
+from watcher import traiter_ticket
+from datetime import datetime
 
-print("Watcher prêt à fonctionner 🎟️")
+
+def lancer_surveillance():
+
+    print("🎟️ Bad Bunny Ticket Watcher démarré")
+    print("Vérification :", datetime.now())
+
+    # TEST TEMPORAIRE
+    # sera remplacé par Ticketmaster / TicketSwap
+
+    tickets = [
+        {
+            "source": "Ticketmaster VIP",
+            "section": "VIP",
+            "prix": 220,
+            "places": 1,
+            "url": "https://www.ticketmaster.be/"
+        }
+    ]
+
+    for ticket in tickets:
+        resultat = traiter_ticket(ticket)
+        print(resultat)
+
+
+if __name__ == "__main__":
+    lancer_surveillance()
