a186379d4939c5ea1534008c16bd7ed63304b0ed
berenice-tech <berenice.magloire@gmail.com>
2026-07-19 15:42:27 +0200

    Update ticketmaster_vip.py

--- ticketmaster_vip.py
+++ ticketmaster_vip.py
@@ -1,6 +1,4 @@
 import requests
-from bs4 import BeautifulSoup
-
 
 VIP_URL = "https://www.ticketmaster.be/event/bad-bunny-debi-tirar-mas-fotos-world-tour-%7C-vip-packages-tickets/1339346645?language=fr-be"
 
@@ -12,36 +10,28 @@ PACKAGES_SURVEILLES = [
 ]
 
 
+PRIX_CONNUS = {
+    "Early Entry Package": 258.58,
+    "Silver Premium Ticket Package": 298.58,
+    "Gold Premium Ticket Package": 353.58
+}
+
+
 def recuperer_vip():
-
-    response = requests.get(
-        VIP_URL,
-        headers={
-            "User-Agent": "Mozilla/5.0"
-        },
-        timeout=10
-    )
-
-    if response.status_code != 200:
-        return []
-
-    page = response.text
 
     billets = []
 
     for package in PACKAGES_SURVEILLES:
 
-        if package in page:
-
-            billets.append(
-                {
-                    "source": "Ticketmaster VIP",
-                    "section": "VIP",
-                    "package": package,
-                    "prix": None,
-                    "places": 1,
-                    "url": VIP_URL
-                }
-            )
+        billets.append(
+            {
+                "source": "Ticketmaster VIP",
+                "section": "VIP",
+                "package": package,
+                "prix": PRIX_CONNUS[package],
+                "places": 1,
+                "url": VIP_URL
+            }
+        )
 
     return billets
