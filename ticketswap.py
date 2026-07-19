59a16f7fc49a1311415c98b8e28d3ec375ec83dc
berenice-tech <berenice.magloire@gmail.com>
2026-07-19 18:43:48 +0200

    Create ticketswap.py

---
+++ ticketswap.py
@@ -0,0 +1,11 @@
+def recuperer_ticketswap():
+
+    return [
+        {
+            "source": "TicketSwap",
+            "section": "3D",
+            "prix": 180,
+            "places": 1,
+            "url": "https://www.ticketswap.com/"
+        }
+    ]
