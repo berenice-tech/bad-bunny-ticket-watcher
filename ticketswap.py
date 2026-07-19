1ff3e6947ac181559b1c05f5e00624af85634aaf
berenice-tech <berenice.magloire@gmail.com>
2026-07-19 20:23:05 +0200

    Update ticketswap.py

--- ticketswap.py
+++ ticketswap.py
@@ -1,3 +1,19 @@
+import requests
+
+
+HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 "
+        "(KHTML, like Gecko) "
+        "Chrome/126.0 Safari/537.36"
+    )
+}
+
+
+TICKETSWAP_URL = "https://www.ticketswap.com/"
+
+
 def recuperer_ticketswap():
 
     return [
