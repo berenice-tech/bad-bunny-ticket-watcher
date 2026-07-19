bcaac66c39031f6c775eba9dfba784431fb550eb
berenice-tech <berenice.magloire@gmail.com>
2026-07-19 18:38:33 +0200

    Update ticketmaster_classique.py

--- ticketmaster_classique.py
+++ ticketmaster_classique.py
@@ -1,21 +1,29 @@
-import requests
+CLASSIQUE_URL = "https://www.ticketmaster.be/event/bad-bunny-debi-tirar-mas-fotos-world-tour-tickets/1117180915?language=fr-be"
 
 
-EVENT_URL = "https://www.ticketmaster.be/event/bad-bunny-debi-tirar-mas-fotos-world-tour-tickets/1117180915?language=fr-be"
+SECTIONS_PRIORITAIRES = [
+    "3D",
+    "3C",
+    "3A",
+    "Golden"
+]
 
 
 def recuperer_classique():
 
-    # Première version :
-    # récupération de la disponibilité publique.
-    # Les sièges détaillés seront ajoutés ensuite.
+    billets = []
 
-    return [
+    # Version initiale de connexion du flux classique
+    # Les vraies disponibilités seront branchées ensuite
+
+    billets.append(
         {
             "source": "Ticketmaster",
             "section": "3D",
             "prix": 145,
             "places": 1,
-            "url": EVENT_URL
+            "url": CLASSIQUE_URL
         }
-    ]
+    )
+
+    return billets
