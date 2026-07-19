7b04135eee7e26fb928004bf1cd6456f9179c033
berenice-tech <berenice.magloire@gmail.com>
2026-07-19 12:34:36 +0200

    Create alerts.py

---
+++ alerts.py
@@ -0,0 +1,20 @@
+def creer_message(
+    niveau,
+    source,
+    section,
+    prix,
+    places,
+    score
+):
+    return f"""
+{niveau} — BAD BUNNY BRUXELLES
+
+🎟 Source : {source}
+📍 Section : {section}
+💺 Places : {places}
+💶 Prix : {prix} €
+
+⭐ Score : {score}
+
+➡️ Vérifie rapidement !
+"""
